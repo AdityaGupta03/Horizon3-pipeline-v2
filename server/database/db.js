da1119b73bf71192d664767cbe3e3d65a2c800ff
Aditya Gupta <adgupta1003@gmail.com>
2024-09-18 22:30:53 -0400

    Integrated server .env file for local db configuration.
    Signed-off-by: Aditya Gupta <adgupta1003@gmail.com>

--- server/database/db.js
+++ server/database/db.js
@@ -1,6 +1,18 @@
 import pg from "pg";
-const { Pool } = pg;
+import dotenv from "dotenv";
 
-let db_pool = new Pool({});
+// Load environment variables from .env file
+dotenv.config();
+
+// Extract database connection details from environment variables
+const { DB_USER, DB_HOST, DB_NAME, DB_PASSWORD } = process.env;
+
+// Create connection to pg database
+let db_pool = new pg.Pool({
+  user: DB_USER,
+  host: DB_HOST,
+  database: DB_NAME,
+  password: DB_PASSWORD,
+});
 
 export { db_pool };
