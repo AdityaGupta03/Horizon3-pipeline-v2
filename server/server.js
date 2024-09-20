38b62f0fcffdac08af94a81e52fc04b5950377e0
Aditya Gupta <adgupta1003@gmail.com>
2024-09-19 21:06:41 -0400

    Removed unnecessary package cors.
    Signed-off-by: Aditya Gupta <adgupta1003@gmail.com>

--- server/server.js
+++ server/server.js
@@ -1,14 +1,7 @@
-import cors from "cors";
 import express from "express";
 import router from "./routes/router.js";
 
 const app = express();
-
-app.use(
-  cors({
-    origin: "http://localhost:3000",
-  }),
-);
 
 app.use(express.json());
 app.use("/api", router);
