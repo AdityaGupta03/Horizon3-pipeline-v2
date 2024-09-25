e95c7840dc296994c0933b5318d6245622401a2b
Srinjoy Dutta <89676505+SrinjoyDutta1@users.noreply.github.com>
2024-09-25 01:37:06 -0400

    frontend signup page done

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import LoginSignup from "./Components/LoginSignup/LoginSignup";
+function App() {
+  return (
+    <div>
+      <LoginSignup/>
+      <h1>
+    </h1>
+    </div>
+
+  );
+}
+
+export default App;
+
+
