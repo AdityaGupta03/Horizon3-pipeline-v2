ce2bad95f1d4c55fa1817302bcc299f4b77baf19
Srinjoy Dutta <89676505+SrinjoyDutta1@users.noreply.github.com>
2024-10-13 13:53:21 -0400

    -bindiff

--- pipeline/binDiff/run.sh
+++
@@ -1,7 +0,0 @@
-cd /Users/mkg/Documents/Horizon3-pipeline2/pipeline/binDiff
-aws s3 cp "$1" ./binaries/binary1
-aws s3 cp "$2" ./binaries/binary2
-ghidra_11.1.2_PUBLIC/support/analyzeHeadless . TestProject -import ./binaries -deleteProject -analysisTimeoutPerFile 100 -scriptPath ./ghidraheadless_binexport/ -postScript ./ghidraheadless_binexport/sample_functions_cpy.py
-bindiff binary1.BinExport binary2.binExport
-rm ./binaries/*
-rm ./*.BinExport
