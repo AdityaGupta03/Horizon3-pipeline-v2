9407d12726eb869bdc636908decfb18259abcf48
Srinjoy Dutta <89676505+SrinjoyDutta1@users.noreply.github.com>
2024-10-23 14:47:54 -0400

    basic template for new frontend

---
+++ sprint2frontend/src/app/accounts/page.tsx
@@ -0,0 +1,5 @@
+const AccountPage = () => {
+  return <div>hello this is the account page</div>;
+};
+
+export default AccountPage;
---
+++ sprint2frontend/src/app/payment/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Payment = () => {
+  return <div>BinaryDiff Page</div>;
+};
+
+export default Payment;
