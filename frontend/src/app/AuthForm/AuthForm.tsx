23df0ba87f8778ab1dcb05c68b06b25574df6d6b
mkg880 <mehrzadgandhi@gmail.com>
2024-12-05 23:10:14 -0500

    small leak fix

--- frontend/src/app/AuthForm/AuthForm.tsx
+++ frontend/src/app/AuthForm/AuthForm.tsx
@@ -155,12 +155,12 @@ const AuthForm: React.FC = () => {
       <div className="overlay-container">
         <div className="overlay">
           <div className="overlay-panel overlay-left">
-            <h1 className="authform">Welcome</h1>
+            <h1 className="welcome">Welcome</h1>
             <p>Have an account? Sign in!</p>
             <button className="ghost" onClick={togglePanel}>Sign In</button>
           </div>
           <div className="overlay-panel overlay-right">
-            <h1 className="authform">Welcome</h1>
+            <h1 className="welcome">Welcome</h1>
             <p>Don't have an account? Sign Up</p>
             <button className="ghost" onClick={togglePanel}>Sign Up</button>
           </div>
